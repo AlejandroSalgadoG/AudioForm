22d09ed2ee4a5c23ee8a52e484e3bf44d65ccf70
Alejandro Salgado G <asalgad2@eafit.edu.co>
2020-05-19 18:06:48 -0500

    Adding first template

--- Audio/views.py
+++ Audio/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello")
+class Index(TemplateView):
+    template = "Index.html"
+
+    def get(self, request):
+        return render(request, self.template, {})
